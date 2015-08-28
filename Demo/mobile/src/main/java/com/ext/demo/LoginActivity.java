47ea9298166c9c1e6e49475e209db43abd1b4058
Abhijeet Bhosale <abhibhosale05@gmail.com>
2015-08-28 16:40:07 +0530

    nfc filter added

--- Demo/mobile/src/main/java/com/ext/demo/LoginActivity.java
+++ Demo/mobile/src/main/java/com/ext/demo/LoginActivity.java
@@ -1,16 +1,24 @@
 package com.ext.demo;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.Arrays;
 
 import cartentia.com.common.manager.JsonRequestHandler;
 import cartentia.com.common.manager.VolleyManager;
@@ -22,19 +30,52 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText username;
     private EditText password;
+    private NfcAdapter mNfcAdapter;
 
+    public static int READER_FLAGS =
+            NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+    private NdefMessage[] msgs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initView();
+        handleIntent(getIntent());
     }
 
     private void initView() {
 
         username = (EditText) findViewById(R.id.usernameEditText);
         password = (EditText) findViewById(R.id.passwordEditText);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+
+        } else {
+            /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                mNfcAdapter.enableReaderMode(this, this, READER_FLAGS, null);
+            }*/
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC Disable.", Toast.LENGTH_LONG).show();
+        } else {
+
+        }
+
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
     }
 
     public void onClick(View view) throws UnsupportedEncodingException {
@@ -53,5 +94,75 @@ public class LoginActivity extends AppCompatActivity {
         });
         VolleyManager.getInstance().addRequestToQueue(request);
 
+    }
+
+    private void handleIntent(Intent intent) {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+
+            for (NdefMessage ndefMessage : msgs) {
+                new NdefReaderTask().execute(ndefMessage);
+            }
+        }
+    }
+
+
+    private class NdefReaderTask extends AsyncTask<NdefMessage, Void, String> {
+
+        @Override
+        protected String doInBackground(NdefMessage... params) {
+
+
+            NdefRecord[] records = params[0].getRecords();
+            for (NdefRecord ndefRecord : records) {
+                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                    try {
+                        return readText(ndefRecord);
+                    } catch (UnsupportedEncodingException e) {
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+        /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+         */
+
+            byte[] payload = record.getPayload();
+
+            // Get the Text Encoding
+            String textEncoding = ((payload[0] & 128) == 0) ? "utf-8" : "utf-16";
+
+            // Get the Language Code
+            int languageCodeLength = payload[0] & 0063;
+
+            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            // e.g. "en"
+
+            // Get the Text
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
